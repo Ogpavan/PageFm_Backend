5967ff07dbbbf02bc12808e4f62d7c2308292735
Ogpavan <pawantwp@gmail.com>
2024-10-06 11:02:08 +0530

    first cisaat

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 // require('dotenv').config();
 // const writersbooks = require('../src/models/writersbooks.js'); 
 const app = express();
@@ -231,4 +231,4 @@ app.get('/', (req, res) => {
 
 
 
-// module.exports = app;
+module.exports = app;
