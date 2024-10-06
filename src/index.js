52b6c0600a1eb96c86dedc409f11ba6d22bee2ef
Ogpavan <pawantwp@gmail.com>
2024-10-06 13:03:56 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 require('dotenv').config();
-const writersbooks = require('./models/writersbooks.js');
+// const writersbooks = require('./models/writersbooks.js');
 
 const app = express();
 
@@ -14,12 +14,10 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Define your routes...
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-// app.listen(3000, () => {
-//   console.log(`Server is running on port 3000`);
-// });
 
 module.exports = app;  // Ensure the app is exported for Vercel
